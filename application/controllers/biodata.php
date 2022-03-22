c54b7402089794cd246ea4966f472f3ed9ba902c
DKvenz <reydhika56@gmail.com>
2022-03-22 11:51:57 +0700

    first commit

---
+++ application/controllers/biodata.php
@@ -0,0 +1,12 @@
+<?php
+class biodata extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Dhika Ardiansyah
+ Saya tingga di cengkareng, jakarta barat
+ olah raga yang saya sukai adalah
+ Sepak bola";
+ }
+}
